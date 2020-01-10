770dff0b52f35348c98b8ebe6c7bade1e5b8c730
Jan Słodkowicz <jan.slod@gmail.com>
2020-01-10 16:22:01 +0100

    corrected flake8 tabs

--- app/core/models.py
+++ app/core/models.py
@@ -5,22 +5,22 @@ from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, \
 
 class UserManager(BaseUserManager):
 
-	def create_user(self, email, password=None, **extra_fields):
-		"""Creates and saves a new user"""
-		user = self.model(email=email, **extra_fields)
-		user.set_password(password)
-		user.save(using=self._db)
+    def create_user(self, email, password=None, **extra_fields):
+        """Creates and saves a new user"""
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
 
-		return user
+        return user
 
 
 class User(AbstractBaseUser, PermissionsMixin):
-	"""Custom user model that supports using email instead of username"""
-	email = models.EmailField(max_length=255, unique=True)
-	name = models.CharField(max_length=255)
-	is_active = models.BooleanField(default=True)
-	is_staff = models.BooleanField(default=False)
+    """Custom user model that supports using email instead of username"""
+    email = models.EmailField(max_length=255, unique=True)
+    name = models.CharField(max_length=255)
+    is_active = models.BooleanField(default=True)
+    is_staff = models.BooleanField(default=False)
 
-	objects = UserManager()
+    objects = UserManager()
 
-	USERNAME_FIELD = 'email'
+    USERNAME_FIELD = 'email'
